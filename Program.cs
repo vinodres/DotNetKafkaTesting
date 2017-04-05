c8abcfe071b6b67bece08e2aeb273a3aa13ef0b9
vinod <vinod.badugu@gmail.com>
2017-04-05 15:45:50 -0400

    Added provider testing code

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestKafkaPublishService
@@ -10,6 +11,11 @@ namespace TestKafkaPublishService
     {
         static void Main(string[] args)
         {
+            var provider = new TestKafkaProvider(CancellationToken.None, "10.79.84.60:9092,10.79.85.103:9092,10.79.85.104:9092", "ESPN.ESD.AdVisor.FakeTest");
+            for (int i = 1; i <= 10; i++)
+            {
+                provider.Publish($"Say Hello {i}", "ESPN.ESD.ADVISOR.TEST.MESSAGE.UPDATE");
+            }
         }
     }
 }
